d1c1f71920f364fd9db7defd38e95f8e4c5cdc02
Michelle Wiginton <michelle.wiginton00@gmail.com>
2021-06-26 17:36:36 -0500

    add python solution for two-sum

---
+++ python/two-sum.py
@@ -0,0 +1,36 @@
+# PROBLEM:
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+# SOLUTION
+def two_sum(nums, target):
+    indices = [0, 0]
+    for i in range(len(nums)):
+        if i != len(nums) - 1:
+            index_to_add = i+1
+            val_to_add = nums[i + 1]
+            if(nums[i] + val_to_add == target):
+                indices[0] = i
+                indices[1] = index_to_add
+                break
+    return indices
+
+        
+
+nums = [3,3]
+target = 6
+print(two_sum(nums, target))
